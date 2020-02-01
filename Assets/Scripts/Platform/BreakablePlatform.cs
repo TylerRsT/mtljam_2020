0e5c1dd48db17536556ffdbefdf87b2390b2b253
Alain Saas <alainsaas@gmail.com>
2020-02-01 09:59:48 -0500

    platformbase initial commit

---
+++ Assets/Scripts/Platform/BreakablePlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : PlatformBase
+{
+    protected override void OnCharacterEnter(Character character)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Platform/PlatformBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlatformBase : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Character character = collision.gameObject.GetComponent<Character>();
+
+        if (character != null)
+        {
+            OnCharacterEnter(character);
+        }
+    }
+    protected abstract void OnCharacterEnter(Character character);
+}
