002a021c3c452af96d7a2adf69540c03e3039dfe
Alain Saas <alainsaas@gmail.com>
2020-02-01 10:54:03 -0500

    added sprite-tile to scene, disable physics and reduce opacity on collision

--- Assets/Scripts/Platform/BreakablePlatform.cs
+++ Assets/Scripts/Platform/BreakablePlatform.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using CreativeSpore.SuperTilemapEditor;
 using UnityEngine;
 
 public class BreakablePlatform : PlatformBase
 {
     protected override void OnCharacterEnter(Character character)
     {
+        Debug.Log("Breakableplatform");
 
+        Sprite sprite = gameObject.GetComponent<Sprite>();
+
+        var rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.simulated = false;
+
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 0.1f);
     }
 }
