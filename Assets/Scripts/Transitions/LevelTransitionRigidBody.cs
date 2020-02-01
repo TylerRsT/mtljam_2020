6ba984ba2e376a533378734cb4a978b5258ae0b2
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-01 15:24:00 -0500

    [Transitions] Added transition system.

---
+++ Assets/Scripts/Transitions/LevelTransitionRigidBody.cs
@@ -0,0 +1,61 @@
+﻿using CreativeSpore.SuperTilemapEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class LevelTransitionRigidBody : MonoBehaviour
+{
+    #region Messages
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="levelID"></param>
+    private void OnTransition(int levelID)
+    {
+        Debug.Assert(_rigidBody != null, "Rigid body not set.");
+        StartCoroutine(ApplyRigidBodyChanges());
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ApplyRigidBodyChanges()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        _rigidBody.simulated = true;
+
+        var tilemapGroup = _rigidBody.GetComponent<TilemapGroup>();
+        if (tilemapGroup != null)
+        {
+            tilemapGroup.enabled = true;
+        }
+    }
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private Rigidbody2D _rigidBody = default;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _colorAlpha = 1.0f;
+
+    #endregion
+}
---
+++ Assets/Scripts/Transitions/LevelTransitionWwiseRTPC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class LevelTransitionWwiseRTPC : MonoBehaviour
+{
+    #region Messages
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="levelID"></param>
+    private void OnTransition(int levelID)
+    {
+        Debug.Assert(!string.IsNullOrWhiteSpace(_rtpcKey), "RTPC Key not set.");
+        AkSoundEngine.SetRTPCValue(_rtpcKey, _rtpcValue);
+    }
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private string _rtpcKey = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _rtpcValue = 0.0f;
+
+    #endregion
+}
