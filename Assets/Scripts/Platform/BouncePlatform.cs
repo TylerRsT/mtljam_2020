6297f4863335e3d94a6515ce701ad20d7f9b0f86
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-01 16:50:04 -0500

    [Platform] Added bouncing platform.

---
+++ Assets/Scripts/Platform/BouncePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class BouncePlatform : PlatformBase
+{
+    #region Override
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="character"></param>
+    protected override void OnCharacterEnter(Character character)
+    {
+        character.SetState(CharacterState.Jumping, _bouncingForce);
+    }
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _bouncingForce = 1.0f;
+
+    #endregion
+}
