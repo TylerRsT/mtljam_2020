61cae32b1f502d95ef8ea52fb0684b8f3ea2898e
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-02 12:25:22 -0500

    Fake transition beginning.

---
+++ Assets/Scripts/Transitions/FakeTransitionTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class FakeTransitionTrigger : MonoBehaviour
+{
+    #region Messages
+
+    private void Start()
+    {
+        _hasTriggered = false;
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_hasTriggered)
+        {
+            gameObject.SendMessage("OnTransition", -1);
+            _hasTriggered = true;
+        }
+    }
+
+    #endregion
+
+    #region Fields
+
+    private bool _hasTriggered = false;
+
+    #endregion
+}
