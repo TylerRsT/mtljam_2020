5ca5c8601be6994e261af8aad57acff559704e2c
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-02 14:21:50 -0500

    Transition Light

---
+++ Assets/Scripts/Transitions/LevelTransitionLight.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class LevelTransitionLight : MonoBehaviour
+{
+    #region Messages
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="levelID"></param>
+    private void OnTransition(int levelID)
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        var fogOfWar = _mainCamera.GetComponent<FogOfWar>();
+        if (fogOfWar != null)
+        {
+            fogOfWar.enabled = _activeState;
+        }
+
+        yield break;
+    }
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private Camera _mainCamera = default;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private bool _activeState = false;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _delay = 2.0f;
+
+    #endregion
+}
---
+++ Assets/Scripts/Transitions/LevelTransitionLightScale.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransitionLightScale : MonoBehaviour
+{
+    #region Messages
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="levelID"></param>
+    private void OnTransition(int levelID)
+    {
+        _started = true;
+        var light = _character.GetComponentInChildren<SpriteRenderer>();
+        if (light == null)
+        {
+            _started = false;
+            return;
+        }
+
+        var origin = light.transform.localScale.x;
+        _step = origin * _duration / _scaleTo;
+    }
+
+    private void Update()
+    {
+        _elapsed += Time.deltaTime;
+
+        if (_elapsed >= _duration)
+        {
+            SetScale(_scaleTo);
+            _started = false;
+            return;
+        }
+
+        var light = _character.GetComponentInChildren<SpriteRenderer>();
+        if(light != null)
+        {
+            SetScale(light.transform.localScale.x + _step);
+        }
+    }
+
+    private void SetScale(float scale)
+    {
+        var light = _character.GetComponentInChildren<SpriteRenderer>();
+        if (light != null)
+        {
+            light.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    private float _scaleTo = 1.0f;
+
+    [SerializeField]
+    private float _duration = 3.0f;
+
+    [SerializeField]
+    private Character _character = default;
+
+    private float _elapsed = 0.0f;
+
+    private bool _started = false;
+
+    private float _step = 0.0f;
+}
