d5c3ce0af12c4db668786fd7d84bd6b61a6f6501
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-01 22:48:27 -0500

    [Platform] Added spline platform.

---
+++ Assets/Scripts/Platform/SplinePlatform.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class SplinePlatform : MonoBehaviour
+{
+    #region Messages
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        var character = collider.GetComponent<Character>();
+        if(character == null)
+        {
+            return;
+        }
+
+        character.SetState(CharacterState.Falling);
+        character.rigidBody.velocity = new Vector2(character.rigidBody.velocity.x, _impulse);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var character = collider.GetComponent<Character>();
+        if (character == null)
+        {
+            return;
+        }
+
+        character.SetState(CharacterState.Falling);
+        character.rigidBody.velocity = new Vector2(character.rigidBody.velocity.x, _exitImpulse);
+    }
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _impulse = 1.0f;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _exitImpulse = 5.0f;
+
+    #endregion
+}
