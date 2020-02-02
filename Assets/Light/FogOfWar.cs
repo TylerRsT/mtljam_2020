69a56ef5d6c627e0602144c23a940bd50150a3d0
Mathias Theys <mathias.theys@viacesi.fr>
2020-02-02 01:05:05 -0500

    [Camera] Added code to enable light around character.

---
+++ Assets/Light/FogOfWar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    RenderTexture _renderTexture = default;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    Shader _shader = default;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _mapOpacity = 0.1f;
+
+    private Material _material;
+    
+    private void Start()
+    {
+        _material = new Material(_shader);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        _material.SetTexture("_LightsTex", _renderTexture);
+        _material.SetFloat("_MapOpacity", _mapOpacity);
+        Graphics.Blit(src, dst, _material);   
+    }
+}
